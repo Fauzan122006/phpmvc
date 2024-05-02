780299e63a1959e5440ce9ac3334dae9a1c4a7e0
Fauzan122006 <fauzantakibang@gmail.com>
2024-05-02 16:40:14 +0800

    add view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,14 +1,26 @@
 <?php
 
-class About
+class About extends Controller
 {
-    public function index($nama = 'Fauzan', $pekerjaan = 'Pelajar')
+    public function index($nama = 'Fauzan', $pekerjaan = 'Pelajar', $umur = 18)
     {
-        echo "Halo saya $nama, saya seorang $pekerjaan";
+
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = "About me";
+
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
+
     }
 }
 
---
+++ app/views/about/index.php
@@ -0,0 +1,3 @@
+<h1>ini adalah halaman about</h1>
+<p>Halo nama saya <?= $data['nama'] ?> saya seorang <?= $data['pekerjaan'] ?> umur saya <?= $data['umur'] ?> tahun
+</p>

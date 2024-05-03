76b44208202a708e0e5f2fa4895586c13b958383
Fauzan122006 <fauzantakibang@gmail.com>
2024-05-03 20:46:03 +0800

    add asset

---
+++ app/core/constant.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/PHP/Sandika%20Galih/phpmvc/public/');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,18 @@
-<h1>ini adalah halaman about</h1>
-<p>Halo nama saya <?= $data['nama'] ?> saya seorang <?= $data['pekerjaan'] ?> umur saya <?= $data['umur'] ?> tahun
-</p>
+<div class="container">
+    <div class="p-5 mb-4  rounded-3 mt-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Selamat datang di Halaman About</h1>
+            <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one
+                in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it
+                to your liking.</p>
+            <button class="btn btn-primary btn-lg" type="button">Example button</button>
+        </div>
+        <div class="aboutlogo-container">
+            <img class="aboutlogo" src="<?= BASEURL; ?>/img/rpl3.png" alt="rpl3-logo">
+        </div>
+
+        <p>Halo nama saya <?= $data['nama'] ?> saya seorang <?= $data['pekerjaan'] ?> umur saya <?= $data['umur'] ?>
+            tahun
+        </p>
+    </div>
+</div>
